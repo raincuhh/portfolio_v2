e0161f45ab6063cf9a4dc1add402a0933c05337a
raincuhh <146904648+raincuhh@users.noreply.github.com>
2024-09-23 13:43:01 +0200

    working on toggling and changing nav menu stuff

--- src/localization.ts
+++ src/localization.ts
@@ -1,3 +1,5 @@
+import { get_id, get_query, create_el, set_el_text_content, set_page_title } from "./helper.js";
+
 enum Languages {
    English = 0,
    Norwegian,
@@ -24,6 +26,10 @@ const translations: {
       [Languages.English]: "white",
       [Languages.Norwegian]: "hvit",
    },
+   "switch language": {
+      [Languages.English]: "switch language",
+      [Languages.Norwegian]: "bytt språk",
+   },
 };
 
 function loc(str: string | null, lang: Languages): string {
@@ -35,4 +41,98 @@ function loc(str: string | null, lang: Languages): string {
    return str;
 }
 
-export { loc, Languages };
+function change_lang(lang: Languages = Languages.English) {
+   // misc
+
+   // section tag
+   let section_tag_List: NodeListOf<Element> | null = document.querySelectorAll(".section_tag");
+   if (section_tag_List) {
+      section_tag_List.forEach((el: Element) => {
+         loc(el.textContent, lang);
+      });
+   }
+
+   // preload
+   let preload_name: Element | null = get_query("#in_cont_preload .preload_title .preload_name strong");
+   if (preload_name) loc(preload_name.textContent, lang);
+
+   let preload_article: Element | null = get_query("#in_cont_preload .preload_article p");
+   if (preload_article) loc(preload_article.textContent, lang);
+
+   let preload_copyright: Element | null = get_query("#in_cont_preload .preload_copyright p");
+   if (preload_copyright) loc(preload_copyright.textContent, lang);
+
+   let preload_wip: Element | null = get_query("#in_cont_preload .preload_wip p");
+   if (preload_wip) loc(preload_wip.textContent, lang);
+
+   // nav
+   let nav_header_name_first: Element | null = get_query(
+      "#nav .nav_wrapper .nav_header .nav_header_name_first"
+   );
+   if (nav_header_name_first) loc(nav_header_name_first.textContent, lang);
+
+   let nav_header_name_last: Element | null = get_query(
+      "#nav .nav_wrapper .nav_header .nav_header_info .nav_header_name_last"
+   );
+   if (nav_header_name_last) loc(nav_header_name_last.textContent, lang);
+
+   let nav_menu_list: Element | null = get_query("#nav .nav_wrapper .nav_menu_list");
+   if (nav_menu_list) {
+      const items: HTMLCollection | null = nav_menu_list.children;
+      if (!items) return;
+
+      for (let i = 0; i < items.length; i++) {
+         const item: Element | null = items[i];
+         let item_header: Element | null = item.querySelector(".nav_menu_item_header");
+         if (!item_header) continue;
+         loc(item_header.textContent, lang);
+         console.log(item_header.textContent);
+      }
+   }
+
+   // scroll nav
+   let scroll_nav_header: Element | null = get_query(
+      "#scroll_nav .scroll_nav_wrapper .scroll_nav_list ._scroll_nav_header"
+   );
+   if (scroll_nav_header) loc(scroll_nav_header.textContent, lang);
+
+   let scroll_nav_list: Element | null = get_query("#scroll_nav .scroll_nav_wrapper .scroll_nav_list");
+   if (scroll_nav_list) {
+      const children: HTMLCollection | null = scroll_nav_list.children;
+      if (!children) return;
+
+      for (let i = 0; i < children.length; i++) {
+         const el: Element | null = children[i];
+         let item: HTMLAnchorElement | null = el.querySelector("scroll_nav_list_item");
+         if (!item) continue;
+         loc(item.textContent, lang);
+      }
+   }
+
+   // intro
+   let intro_title_text_line_list = get_query("#in_cont_intro");
+   if (intro_title_text_line_list) {
+      const children: HTMLCollection | null = intro_title_text_line_list.children;
+      if (!children) return;
+
+      for (let i = 0; i < children.length; i++) {
+         const el: Element | null = children[i];
+         let strong_el = el.querySelector("strong");
+         if (!el || strong_el) return;
+
+         if (el && strong_el) {
+            loc(strong_el.textContent, lang);
+         } else {
+            loc(el.textContent, lang);
+         }
+      }
+   }
+
+   // about
+
+   // projects
+
+   // contact
+}
+
+export { loc, Languages, change_lang };
--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,7 @@
-import { loc, Languages } from "./localization.js";
-import { change_lang } from "./translation.js";
+import { loc, change_lang, Languages } from "./localization.js";
 import { get_id, get_query, create_el, set_el_text_content, set_page_title } from "./helper.js";
 
 // nav
-let nav_visiblity: boolean = false;
-let nav_title: Element | null = get_query("#nav .nav_wrapper .nav_title");
-
-if (nav_title) {
-   nav_title.addEventListener("click", () => {
-      toggle_nav();
-   });
-}
-
-function toggle_nav() {
-   let nav_list: Element = get_query("#nav .nav_wrapper .nav_list");
-
-   if (window.innerWidth >= 1024) {
-      nav_list.setAttribute("style", "display: none");
-      nav_visiblity = false;
-      return;
-   }
-
-   if (!nav_visiblity) {
-      nav_visiblity = true;
-      nav_list.setAttribute("style", "display: block");
-   } else {
-      nav_visiblity = false;
-      nav_list.setAttribute("style", "display: none");
-   }
-}
-
-document.addEventListener("resize", () => {
-   check_resize_on_nav_list();
-});
-
-function check_resize_on_nav_list() {
-   if (window.innerWidth >= 1024 && nav_visiblity) {
-      let nav_list: Element = get_query("#nav .nav_wrapper .nav_list");
-
-      nav_visiblity = false;
-      nav_list.setAttribute("style", "display: none");
-   }
-}
-
 enum nav_menu_state {
    contact = "contact",
    theme = "theme",
@@ -50,10 +9,8 @@ enum nav_menu_state {
 }
 
 let current_nav_menu_state: nav_menu_state = nav_menu_state.contact;
-
-function update_nav_menu_visiblity(_nav_menu_state: nav_menu_state) {
+function show_single_nav_menu_category(_nav_menu_state: nav_menu_state): void {
    const nav_menu_list: Element | null = get_query("#nav .nav_wrapper .nav_menu .nav_menu_list");
-
    if (!nav_menu_list) return;
 
    const nav_menu_categories = {
@@ -66,24 +23,98 @@ function update_nav_menu_visiblity(_nav_menu_state: nav_menu_state) {
 
    const selected_cat: Element | null = nav_menu_categories[_nav_menu_state];
    selected_cat?.setAttribute("style", "display: block");
-   console.log(selected_cat);
+}
+
+function toggle_nav_menu_list_options(visible: boolean) {
+   const nav_menu_list: Element | null = get_query("#nav .nav_wrapper .nav_menu .nav_menu_list");
+   if (!nav_menu_list) return;
+
+   const nav_menu_categories = {
+      contact: nav_menu_list.querySelector("#nav_menu_item_contact"),
+      theme: nav_menu_list.querySelector("#nav_menu_item_theme"),
+      lang: nav_menu_list.querySelector("#nav_menu_item_lang"),
+   };
+   if (!nav_menu_categories) return;
+
+   const nav_menu_category_header = {
+      contact_header: nav_menu_categories.contact.querySelector("#nav_menu_header_contact"),
+      theme_header: nav_menu_categories.theme.querySelector("#nav_menu_header_theme"),
+      lang_header: nav_menu_categories.theme.querySelector("#nav_menu_header_lang"),
+   };
+   if (!nav_menu_category_header) return;
+
+   const nav_menu_category_body = {
+      contact_body: nav_menu_categories.contact.querySelector(".nav_menu_item_cont"),
+      theme_body: nav_menu_categories.theme.querySelector(".nav_menu_item_cont"),
+      lang_body: nav_menu_categories.theme.querySelector(".nav_menu_item_cont"),
+   };
+   if (!nav_menu_category_body) return;
+
+   switch (visible) {
+      case true:
+         Object.values(nav_menu_categories).forEach((el) => el?.setAttribute("style", "display: block"));
+         Object.values(nav_menu_category_header).forEach((el: Element) => {
+            el?.setAttribute("style", "display: block");
+         });
+         Object.values(nav_menu_category_body).forEach((el: Element) => {
+            el?.setAttribute("style", "display: none");
+         });
+         break;
+      case false:
+         Object.values(nav_menu_categories).forEach((el) => el?.setAttribute("style", "display: block"));
+         Object.values(nav_menu_category_header).forEach((el: Element) => {
+            el?.setAttribute("style", "display: none");
+         });
+         Object.values(nav_menu_category_body).forEach((el: Element) => {
+            el?.setAttribute("style", "display: none");
+         });
+         break;
+   }
+}
+
+let nav_menu_item_headers: NodeListOf<Element> | null = document.querySelectorAll(
+   "#nav .nav_wrapper .nav_menu .nav_menu_list .nav_menu_item .nav_menu_item_header"
+);
+if (nav_menu_item_headers) {
+   const cats: Array<string> = ["nav_menu_header_contact", "nav_menu_header_theme", "nav_menu_header_lang"];
+
+   nav_menu_item_headers.forEach((el: Element) => {
+      el.addEventListener("onclick", () => {
+         let el_id: string | null = el.getAttribute("id");
+
+         if (cats.includes(el_id)) {
+            toggle_nav_menu_list_options(true);
+         }
+      });
+   });
 }
 
 function set_default_nav_menu_visiblity() {
-   update_nav_menu_visiblity(current_nav_menu_state);
+   show_single_nav_menu_category(current_nav_menu_state);
 }
 
 // language
 
-function set_default_lang() {
+function set_default_lang(): void {
    change_lang(Languages.English);
 }
 
-function set_copyright_year() {}
+function set_copyright_year(): void {
+   let date: Date = new Date();
+   let year: number = date.getFullYear();
 
-function main() {
+   let copyright_year_divs: NodeListOf<Element> | null = document.querySelectorAll("#copyright_year");
+   if (copyright_year_divs) {
+      copyright_year_divs.forEach((e: Element) => {
+         e.textContent = year.toString();
+      });
+   }
+}
+
+function main(): void {
    set_default_nav_menu_visiblity();
    set_default_lang();
+   set_copyright_year();
 }
 
 export { main };
