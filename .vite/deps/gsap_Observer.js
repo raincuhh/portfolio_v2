35608cb9b769170567b6558d6b07cbdbda3d61b6
Filip Ryan <filipryanbusinessmail@gmail.com>
2024-09-28 01:08:06 +0200

    Updates

---
+++ .vite/deps/gsap_Observer.js
@@ -0,0 +1,26 @@
+import {
+  Observer,
+  _getProxyProp,
+  _getScrollFunc,
+  _getTarget,
+  _getVelocityProp,
+  _horizontal,
+  _isViewport,
+  _proxies,
+  _scrollers,
+  _vertical
+} from "./chunk-BC5G4GQD.js";
+export {
+  Observer,
+  _getProxyProp,
+  _getScrollFunc,
+  _getTarget,
+  _getVelocityProp,
+  _horizontal,
+  _isViewport,
+  _proxies,
+  _scrollers,
+  _vertical,
+  Observer as default
+};
+//# sourceMappingURL=gsap_Observer.js.map
